01a082421b7242119d958f592a73a734dae10507
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-05-12 04:24:30 -0300

    app modifications (link routes and stylesheet)

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from "./components/NavBar"
 import HomeContainer from "./components/HomeContainer"
-import HouseListContainer from "./components/HousesContainer"
+import HousesContainer from "./components/HousesContainer"
 import HouseDetailContainer from "./components/HouseDetailContainer"
 import Wizards from "./components/Wizards"
 import Elixirs from "./components/Elixirs"
 import Ingredients from "./components/Ingredients"
 import Spoils from "./components/Spoils"
 import Feedback from "./components/Feedback"
-import './App.css'
+import './components/components.css'
 
 function App() {
   return (
     <>
-    <BrowserRouter> 
-      <NavBar/>
-      <Routes>
-        <Route exact path='/' element={<HomeContainer/>}/>
-        <Route exact path='/houses' element={<HouseListContainer/>}/>
-        <Route exact path='/houses/:id' element={<HouseDetailContainer/>}/>
-        <Route exact path='/wizards' element={<Wizards/>}/>
-        <Route exact path='/elixirs' element={<Elixirs/>}/>
-        <Route exact path='/ingredients' element={<Ingredients/>}/>
-        <Route exact path='/spoils' element={<Spoils/>}/>
-        <Route exact path='/feedback' element={<Feedback/>}/>
-      </Routes>
-    </BrowserRouter>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route exact path='/' element={<HomeContainer/>}/>
+          <Route exact path='/houses' element={<HousesContainer/>}/>
+          <Route exact path='/houses/:id' element={<HouseDetailContainer/>}/>
+          <Route exact path='/wizards' element={<Wizards/>}/>
+          <Route exact path='/elixirs' element={<Elixirs/>}/>
+          <Route exact path='/ingredients' element={<Ingredients/>}/>
+          <Route exact path='/spoils' element={<Spoils/>}/>
+          <Route exact path='/feedback' element={<Feedback/>}/>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
