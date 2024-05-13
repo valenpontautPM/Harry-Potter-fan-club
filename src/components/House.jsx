30385f6943881247594f71df08ab5543062bff68
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-05-12 23:35:14 -0300

    first amplitude implementation :D

--- src/components/House.jsx
+++ src/components/House.jsx
@@ -1,15 +1,21 @@
 import { Container, Card, Button, Text,  Image } from '@chakra-ui/react'
 import { Link } from "react-router-dom"
 import PropTypes from 'prop-types'
+import * as amplitude from '@amplitude/analytics-browser';
+const AMPLITUDE_API_KEY = "986ab970a868fbebc01877a2b9d342ab"
+amplitude.init(AMPLITUDE_API_KEY);
 
 const House = ({id, name, color_cod, img}) => {
+    const handleVisitHouse = () => {
+      amplitude.track('Visit House', { nameHouse: name });
+    };
     return (
         <Container className='house__container'>  
           <Card className='house__container__card' bg={color_cod}>
             <Text className='house__container__card__title'>{name.toUpperCase()}</Text>
             <Image className='house__container__card__image' src={img}/>
             <Link className='house__container__card__button' to={`/houses/${id}`}>
-              <Button color={color_cod}>
+              <Button color={color_cod} onClick={handleVisitHouse}>
                 Visit House
               </Button>
             </Link>
