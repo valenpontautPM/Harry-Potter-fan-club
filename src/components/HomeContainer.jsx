997b8e20a48f12215fa1085a9b7c1e8cf99c6c69
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2024-05-14 23:11:43 -0300

    fix event tracking

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -16,7 +16,7 @@ const HomeContainer = () => {
             <Flex className="home__container__flex">
                 <div className='home__container__flex__div'>
                     <Text className="home__container__flex__div__title">Welcome to the Wizarding World Fan Club! </Text>
-                    <Text className="home__container__flex__div__bodytext">Step into the magical world of Harry Potter and join fellow fans in celebrating J.K. Rowling&apos;s famous series. Explore Hogwarts houses, meet wizards, discover potions, ingredients, and spells, and share your thoughts in our <Link className="home__link" to={`/feedback`} onClick={handleClickVisitFeedback()}>feedback section</Link></Text>
+                    <Text className="home__container__flex__div__bodytext">Step into the magical world of Harry Potter and join fellow fans in celebrating J.K. Rowling&apos;s famous series. Explore Hogwarts houses, meet wizards, discover potions, ingredients, and spells, and share your thoughts in our <Link className="home__link" to={`/feedback`} onClick={handleClickVisitFeedback}>feedback section</Link></Text>
                     <Text className="home__container__flex__div__endtext">Your journey into the magical world awaits!</Text>
                 </div>
                 <Image className='home__container__flex__image' src={shield}/>
